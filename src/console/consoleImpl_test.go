5f81a9dd35da63454789eae209d7b547a5080781
Panachai <panachai.ny@gmail.com>
2025-08-14 00:06:35 +0700

    test(console): add initial test structure for ConsoleImpl

---
+++ src/console/consoleImpl_test.go
@@ -0,0 +1,15 @@
+package console
+
+import (
+	"testing"
+)
+
+func TestConsoleImpl_Basic(t *testing.T) {
+	// TODO: Add meaningful tests for ConsoleImpl methods
+	// Example:
+	// c := NewConsoleImpl()
+	// err := c.SomeMethod()
+	// if err != nil {
+	//     t.Errorf("unexpected error: %v", err)
+	// }
+}
