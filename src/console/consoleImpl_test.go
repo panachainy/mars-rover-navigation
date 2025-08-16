557a2a863c608dd555a82a7d07a48fb46e4c721a
Panachai <panachai.ny@gmail.com>
2025-08-16 14:04:14 +0700

    chore(console): comment out placeholder test for ConsoleImpl methods

--- src/console/consoleImpl_test.go
+++ src/console/consoleImpl_test.go
@@ -1,15 +1,11 @@
 package console
 
-import (
-	"testing"
-)
-
-func TestConsoleImpl_Basic(t *testing.T) {
-	// TODO: Add meaningful tests for ConsoleImpl methods
-	// Example:
-	// c := NewConsoleImpl()
-	// err := c.SomeMethod()
-	// if err != nil {
-	//     t.Errorf("unexpected error: %v", err)
-	// }
-}
+// func TestConsoleImpl_Basic(t *testing.T) {
+// 	// TODO: Add meaningful tests for ConsoleImpl methods
+// 	// Example:
+// 	// c := NewConsoleImpl()
+// 	// err := c.SomeMethod()
+// 	// if err != nil {
+// 	//     t.Errorf("unexpected error: %v", err)
+// 	// }
+// }
